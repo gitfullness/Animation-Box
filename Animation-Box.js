7c7f0a3ae9c903eec1de278df2ee427fa2d7fc49
gitfullness <118089871+gitfullness@users.noreply.github.com>
2023-01-05 19:42:51 -0800

    Create Animation-Box.js

---
+++ Animation-Box.js
@@ -0,0 +1,32 @@
+/*
+Animation Box
+
+coded by Julia Hiyeon Kim (c)2021
+
+*/
+
+window.onload = function() {
+	var box = document.getElementById('box');
+	var posX = 0;
+
+	var tX = setInterval(moveX, 8);
+	function moveX() {
+		if (posX == 150) {
+			var tX = setInterval(moveX, 4);
+			function moveX() {
+				if (posX == 0) {
+					clearInterval(tX);
+				}
+				else {
+					posX = posX - 1;
+					box.style.left = posX + 'px';
+				}
+			}
+		}
+		else {
+			posX = posX + 1;
+			box.style.left = posX + 'px';
+		}
+	}
+
+};
